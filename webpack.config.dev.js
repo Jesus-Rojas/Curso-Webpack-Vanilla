faa9c2e0cb4f4d1563105cb357f53cdfe1c15e10
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-12 01:49:32 -0500

    hot reload

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -92,5 +92,11 @@ module.exports = {
       new CssMinimizerPlugin(),
       new TerserPlugin(),
     ],
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
   }
 }
